e4d8a8e3c21631ff8681287f1c6f37417f003335
Kristof Herrmann <kristof.herrmann@rwth-aachen.de>
2023-04-14 17:00:34 +0200

    feat: add logger and retries

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import json
 import os
 from pathlib import Path
-from github_client import GithubClient
-from config import GH_ACCESS_TOKEN, OPENAI_KEY
 import structlog
+import tenacity
 
+from config import GH_ACCESS_TOKEN, OPENAI_KEY
+from github_client import GithubClient
 from pipeline import GithubEventPrompter, SummaryPrompter
 
-logger = structlog.get_logger()
+logger = structlog.get_logger(__name__)
+
 
 # set this to a selected date between today and a couple of days back (limited by GH API pagination limit)
 DATE = os.getenv("DATE", "2023-04-13")
@@ -21,16 +23,29 @@ prompter_summaries = SummaryPrompter(OPENAI_KEY, Path("./prompts/summary.txt"))
 def generate_events():
     results = {}
     events = github_client.get_events(USERNAME, DATE)
+    logger.info("Start summarizing github events")
     for event, payload in events.items():
-        result = prompter_events.query(event=event, input=json.dumps(payload))
-        results[event] = {
-            "input": payload,
-            "output": result[0],
-        }
-        logger.info("Updating result example  with event ", event=event)
-        # write strigified results to file 'DATE.json' in /examples
-        with open(f"./examples/{DATE}.json", "w") as f:
-            f.write(json.dumps(results, indent=4))
+        logger.info(
+            "Summarizing events category", event_category=event, num_events=len(payload)
+        )
+        for attempt in tenacity.Retrying(
+            stop=tenacity.stop_after_attempt(3),
+            wait=tenacity.wait_exponential(1),
+            reraise=True,
+        ):
+            with attempt:
+                result = prompter_events.query(event=event, input=json.dumps(payload))
+                results[event] = {
+                    "input": payload,
+                    "output": result[0],
+                }
+                logger.info(
+                    "Updating result example with event ",
+                    event_category=event,
+                    num_events=len(payload),
+                )
+                with open(f"./examples/{DATE}.json", "w") as f:
+                    f.write(json.dumps(results, indent=4))
     logger.info("Done generating event summaries", date=DATE)
 
 
